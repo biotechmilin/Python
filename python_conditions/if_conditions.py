01751fabf6854c3b984abd419a3a931041c531ac
Milin <biotechmilin@gmail.com>
2024-08-25 21:55:01 -0400

    this is conditional statement practice

---
+++ python_conditions/if_conditions.py
@@ -0,0 +1,20 @@
+"""
+equals : a==b
+not equals : a=!b
+less than : a < b
+less than or equal to: a <= b
+greater than : a > b
+greater than or equal to: a >= b
+"""
+
+a = 33
+b = 222
+if a < b:
+    print("a is less than b")
+print('***********************************')
+
+a = 33
+b = 222
+if b < a:
+
+    print("a is less than b") # IndentationError: expected an indented block after 'if' statement on line 18
