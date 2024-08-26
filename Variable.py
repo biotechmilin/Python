9c5cabbca1b05c70ed3c0f062340bae26ba14960
Milin <biotechmilin@gmail.com>
2024-08-25 20:45:08 -0400

    this is variables

--- Variable.py
+++ Variable.py
@@ -1,4 +1,32 @@
+# python has no command to declare variables
 a = 10
 b = 20
 c = a + b
 print(c)
+print('*************************************************')
+
+# Variables does not need to be declared with specific type of it.
+x = 5
+y = "John"
+print(x)
+print(y)
+print('*************************************************')
+# variable can be changed ones its declared and it is case-sensitive
+x = 5
+x = "hello"
+X = 30.9
+print(x)
+print(X)
+# casting
+print('*************************************************')
+r = str(3) # r will be '3'
+t = int(3) # t will be 3
+u = float(3) # u will be 3.0
+print(r,t,u)
+print('*************************************************')
+# you can get the type variable
+e = 4.0
+f= 'hello'
+print(type(e))
+print(type(f))
+print('*************************************************')
